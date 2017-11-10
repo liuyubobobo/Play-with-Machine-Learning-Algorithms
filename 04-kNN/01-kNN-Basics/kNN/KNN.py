ce15aa9f4ae96fe75a2cc2e2def287718dd2471a
liuyubobobo <liuyubobobo@gmail.com>
2017-11-09 21:46:51 -0800

    Chapter 04 section 02 updated.

--- 04-kNN/01-kNN-Basics/kNN/KNN.py
+++ 04-kNN/01-kNN-Basics/kNN/KNN.py
@@ -6,8 +6,10 @@ from collections import Counter
 def kNN_classify(k, X_train, y_train, x):
 
     assert 1 <= k <= X_train.shape[0], "k must be valid"
-    assert X_train.shape[0] == y_train.shape[0], "the size of X_train must equal to the size of y_train"
-    assert X_train.shape[1] == x.shape[0], "the feature number of x must be fitted"
+    assert X_train.shape[0] == y_train.shape[0], \
+        "the size of X_train must equal to the size of y_train"
+    assert X_train.shape[1] == x.shape[0], \
+        "the feature number of x must be equal to X_train"
 
     distances = [(sqrt(((x_train - x)**2).sum()), y_train[i])
                  for i, x_train in enumerate(X_train)]
--- 04-kNN/02-kNN-in-scikit-learn/kNN/KNN.py
+++ 04-kNN/02-kNN-in-scikit-learn/kNN/KNN.py
@@ -1,19 +1,50 @@
 import numpy as np
+from math import sqrt
+from collections import Counter
+
 
 class KNN:
 
     def __init__(self, k):
+        """初始化kNN分类器"""
+        assert k >= 1, "k must be valid"
         self.k = k
+        self._X_train = None
+        self._y_train = None
 
-    def fit(self, train_X, train_y):
-        self._train_X = train_X
-        self._train_y = train_y
+    def fit(self, X_train, y_train):
+        """根据训练数据集X_train和y_train训练kNN分类器"""
+        assert X_train.shape[0] == y_train.shape[0], \
+            "the size of X_train must be equal to the size of y_train"
+        assert self.k <= X_train.shape[0], \
+            "the size of X_train must be at least k."
+
+        self._X_train = X_train
+        self._y_train = y_train
         return self
 
-    def predict(self, test_X):
-        '''
-        
-        :param test_X: 
-        :return: 
-        '''
-        pass
+    def predict(self, X_predict):
+        """给定待预测数据集X_predict，返回表示X_predict的结果向量"""
+        assert self._X_train is not None and self._X_train is not None, \
+                "must fit before predict!"
+        assert X_predict.shape[1] == self._X_train.shape[1], \
+                "the feature number of X_predict must be equal to X_train"
+
+        y_predict = [self._predict(x) for x in X_predict]
+        return np.array(y_predict)
+
+    def _predict(self, x):
+        """给定单个待预测数据x，返回x_predict的预测结果值"""
+        assert x.shape[0] == self._X_train.shape[1], \
+            "the feature number of x must be equal to X_train"
+        distances = [(sqrt(((x_train - x) ** 2).sum()), self._y_train[i])
+                     for i, x_train in enumerate(self._X_train)]
+        distances.sort()
+
+        topK_y = [pair[1] for pair in distances[:self.k]]
+
+        votes = Counter(topK_y)
+        return votes.most_common(1)[0][0]
+
+    def __repr__(self):
+        return "KNN(k=%d)" % self.k
