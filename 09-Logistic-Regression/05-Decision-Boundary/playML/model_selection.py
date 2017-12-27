c4a5cab72b7728654ef88220e4638928c4ae6c67
liuyubobobo <liuyubobobo@gmail.com>
2017-12-27 04:38:26 -0800

    Chapter 09 section 05 added.

---
+++ 09-Logistic-Regression/05-Decision-Boundary/playML/model_selection.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def train_test_split(X, y, test_ratio=0.2, seed=None):
+    """将数据 X 和 y 按照test_ratio分割成X_train, X_test, y_train, y_test"""
+    assert X.shape[0] == y.shape[0], \
+        "the size of X must be equal to the size of y"
+    assert 0.0 <= test_ratio <= 1.0, \
+        "test_ration must be valid"
+
+    if seed:
+        np.random.seed(seed)
+
+    shuffled_indexes = np.random.permutation(len(X))
+
+    test_size = int(len(X) * test_ratio)
+    test_indexes = shuffled_indexes[:test_size]
+    train_indexes = shuffled_indexes[test_size:]
+
+    X_train = X[train_indexes]
+    y_train = y[train_indexes]
+
+    X_test = X[test_indexes]
+    y_test = y[test_indexes]
+
+    return X_train, X_test, y_train, y_test
